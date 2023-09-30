ab55ff373629840dc35f0c555983a1533a6a6aae
Lucia Avila E <126265797+LAE1201Soo@users.noreply.github.com>
2023-09-30 13:35:38 -0600

    Login

--- app/src/main/java/com/example/gsangin/MainActivity.kt
+++ app/src/main/java/com/example/gsangin/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.gsangin
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -12,34 +14,38 @@ class MainActivity : AppCompatActivity() {
     private lateinit var userName: EditText
     private lateinit var Contraseñatxt: EditText
     private lateinit var Ingresartxt: Button
-    private lateinit var txtRegis: TextView
+    private lateinit var btnAbrirSegundaActividad: Button
+    private lateinit var sharedPreferences: SharedPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        sharedPreferences = getSharedPreferences("MisPreferencias", Context.MODE_PRIVATE)
 
-        txtRegis = findViewById(R.id.crearUser)
         userName = findViewById(R.id.txtUser)
         Contraseñatxt = findViewById(R.id.txtContraseña)
         Ingresartxt = findViewById(R.id.btnIngresar)
+        btnAbrirSegundaActividad = findViewById(R.id.btnRegis)
+
         Ingresartxt.setOnClickListener {
             val username = userName.text.toString()
             val password = Contraseñatxt.text.toString()
 
+            val savedUsername = sharedPreferences.getString("usuario", "")
+            val savedPassword = sharedPreferences.getString("contraseña", "")
 
-            if (username == "Kyung1201" && password == "Exo0t09D") {
-
+            if (username == savedUsername && password == savedPassword) {
                 Toast.makeText(this, "Bienvenido", Toast.LENGTH_SHORT).show()
 
+                val intent = Intent(this, MenuPrincipal::class.java)
+                startActivity(intent)
             } else {
-
                 Toast.makeText(this, "Usuario o Contraseña Incorrecta", Toast.LENGTH_SHORT).show()
             }
+        }
 
-            txtRegis.setOnClickListener {
-                // Agregar aquí el código para abrir la segunda actividad
-                val intent = Intent(this, RegiUser::class.java)
-                startActivity(intent)
-            }
-    }
-
+        btnAbrirSegundaActividad.setOnClickListener {
+            val intent = Intent(this, RegiUser::class.java)
+            startActivity(intent)
+        }
     }}
--- app/src/main/java/com/example/gsangin/RegiUser.kt
+++ app/src/main/java/com/example/gsangin/RegiUser.kt
@@ -1,11 +1,58 @@
 package com.example.gsangin
 
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class RegiUser : AppCompatActivity() {
+    private lateinit var nombreEditText: EditText
+    private lateinit var usuarioEditText: EditText
+    private lateinit var contraseñaEditText: EditText
+    private lateinit var confirmarContraseñaEditText: EditText
+    private lateinit var guardarButton: Button
+    private lateinit var sharedPreferences: SharedPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_regi_user)
+        sharedPreferences = getSharedPreferences("MisPreferencias", Context.MODE_PRIVATE)
+
+        nombreEditText = findViewById(R.id.txtNombre)
+        usuarioEditText = findViewById(R.id.txtUsuario)
+        contraseñaEditText = findViewById(R.id.txtPass)
+        confirmarContraseñaEditText = findViewById(R.id.txtPassCom)
+        guardarButton = findViewById(R.id.btnGuardar)
+
+        guardarButton.setOnClickListener {
+            val nombre = nombreEditText.text.toString()
+            val usuario = usuarioEditText.text.toString()
+            val contraseña = contraseñaEditText.text.toString()
+            val confirmarContraseña = confirmarContraseñaEditText.text.toString()
+
+            // Verificar que la contraseña y la confirmación de contraseña sean iguales
+            if (contraseña == confirmarContraseña) {
+                // Las contraseñas coinciden, guardar los datos en las preferencias compartidas
+                val editor = sharedPreferences.edit()
+                editor.putString("nombre", nombre)
+                editor.putString("usuario", usuario)
+                editor.putString("contraseña", contraseña)
+                editor.apply()
+
+                Toast.makeText(this, "Datos guardados", Toast.LENGTH_SHORT).show()
+
+                // Limpiar los campos
+                nombreEditText.text.clear()
+                usuarioEditText.text.clear()
+                contraseñaEditText.text.clear()
+                confirmarContraseñaEditText.text.clear()
+            } else {
+                // Las contraseñas no coinciden, mostrar un mensaje de error
+                Toast.makeText(this, "Las contraseñas no coinciden", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
-}
+        }
