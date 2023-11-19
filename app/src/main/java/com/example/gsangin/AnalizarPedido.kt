ccb760fa1a865cb7220c5b1bbb97599663d7f18b
LAE1201Soo <te200111566@tequila.tecmm.edu.mx>
2023-11-19 12:01:53 -0600

    ya con boton

--- app/src/main/java/com/example/gsangin/AnalizarPedido.kt
+++ app/src/main/java/com/example/gsangin/AnalizarPedido.kt
@@ -2,10 +2,19 @@ package com.example.gsangin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.gsangin.model.ProductoSQLiteModel
 
 class AnalizarPedido : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_analizar_pedido)
+
+        // Obtén la lista de productos seleccionados y sus cantidades del Intent
+        val productosSeleccionados =
+            intent.getSerializableExtra("productosSeleccionados") as? ArrayList<ProductoSQLiteModel>
+                ?: throw IllegalArgumentException("Lista de productos no proporcionada en el intent")
+
+        val cantidades =
+            intent.getIntegerArrayListExtra("cantidades") ?: throw IllegalArgumentException("Lista de cantidades no proporcionada en el intent")
     }
 }
