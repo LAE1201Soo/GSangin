3933566fcc1bf4137f7de2f47768dc8747550d11
LAE1201Soo <te200111566@tequila.tecmm.edu.mx>
2023-11-20 13:52:21 -0600

    muestra total en analizar pedido y te manda la alarma michi

--- app/src/main/java/com/example/gsangin/AnalizarPedido.kt
+++ app/src/main/java/com/example/gsangin/AnalizarPedido.kt
@@ -1,17 +1,26 @@
 package com.example.gsangin
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.gsangin.model.ProductoSQLiteModel
 import com.example.gsangin.model.ProductosSeleccionadosAdapter
 
 class AnalizarPedido : AppCompatActivity() {
+    private  lateinit var  btnComfirmar: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_analizar_pedido)
+
+        // iniciar el boton de comfirmar
+        btnComfirmar= findViewById(R.id.btComfirmsrPP)
+
 
         // Obtén la información del cliente del Intent
         val numeroCliente = intent.getIntExtra("numeroCliente", 0)
@@ -55,5 +64,18 @@ class AnalizarPedido : AppCompatActivity() {
 // Muestra el subtotal y el total en los TextViews
         subtotalTextView.text = "Subtotal: $subtotalRedondeado"
         totalTextView.text = "Total con ipes e iva : $totalRedondeado"
+
+        // btn congfirmare
+        btnComfirmar.setOnClickListener {
+            val mensaje = "Funcion no disponible " +
+                    "entretente gustavo  "
+            val tostada = Toast.makeText(this, mensaje, Toast.LENGTH_SHORT)
+            tostada.show()
+            // Redirigir a tu enlace de TikTok
+            val enlaceTikTok = "https://vm.tiktok.com/ZMjEMqMSR/"
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(enlaceTikTok))
+            startActivity(intent)
+        }
     }
+
 }
